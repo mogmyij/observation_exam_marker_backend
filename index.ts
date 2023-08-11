8a1837aafdbcba169f133b35f921c7b0d90ecd8c
mogmyij <foonglucas@gmail.com>
2023-08-11 12:54:34 +0800

    added .gitignore

--- index.ts
+++ index.ts
@@ -3,9 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const app=express();
 
+const databasePassword=process.env.DATABASE_PASSWORD
+const databaseUrl=`mongodb+srv://mogmyij:${databasePassword}@observationlevel1.5kkcv9y.mongodb.net/?retryWrites=true&w=majority`
 
+//responds with the app when root of website is visited
+app.use(express.static("build"))
 
 const PORT=process.env.PORT
 app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
+    console.log(`backend server running on port ${PORT}`);
 })
