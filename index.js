9cf2300ca249f15b65c63916b607d881606e62e8
mogmyij <foonglucas@gmail.com>
2023-10-08 00:21:05 +0800

    added admin static page

--- index.js
+++ index.js
@@ -14,52 +14,8 @@ const userScore = require("./models/userScore");
 //express middleware (executes after getting request)
 app.use(express.json());
 app.use(cors());
-/*
-const testUserScore = new UserScore({
-	id: 0,
 
-	question1a1Score: 5,
-	question1a2Score: 0,
-	question1a3Score: 0,
-	question1c1Score: 0,
-	question1c2Score: 0,
-	question1c3Score: 0,
-
-	question2b1: 0,
-	question2b2: 0,
-	question2b3: 0,
-	question2b4: 0,
-	question2b5: 0,
-	question2b6: 0,
-	question2b7: 0,
-	question2b8: 0,
-
-	question3c1: 0,
-	question3c2: 0,
-	question3c3: 0,
-	question3c4: 0,
-
-	question4b1: 0,
-	question4c1: 0,
-
-	question1TEFScore: 0,
-	question2TEFScore: 0,
-	question3TEFScore: 0,
-	question4TEFScore: 0,
-	question5TEFScore: 0,
-
-	totalScore: 0,
-	user: "64e42f59fbd2a5c629374948",
-});
-
-testUserScore.save()*/
-
-const testVirtual = User.findById("64e42f59fbd2a5c629374948")
-	.populate("userScore")
-	.then((response) => {
-		console.log(response.userScore);
-	});
-
+//--------------------- API ROUTES ---------------------------//
 //add user most often used when frontend login page is submitted
 app.post("/api/users", (request, response) => {
 	const newUser = new User(request.body);
@@ -75,16 +31,9 @@ app.post("/api/users", (request, response) => {
 
 //get list of all userScores by populating the virtuals of the user documents
 app.get("/api/userScore", async (request, response) => {
-	/*const users = await user.find();
-	const scores = await Promise.all(
-		users.map(async (curr) => {
-			const populatedUser = await curr.populate("userScore");
-			return populatedUser.userScore;
-		})
-	);
-	return response.json(scores);*/
 	const userScores = await userScore
 		.find()
+		.sort({_id: -1})
 		.populate({ path: "user", select: "name nric -_id" });
 	return response.json(userScores);
 });
@@ -100,9 +49,14 @@ app.put("/api/users/:id", (request, response) => {
 
 //---- STATIC ROUTES -----//
 //responds with the app when root of website is visited
-app.use(express.static("build"));
+app.use(express.static("staticRoutes/adminSite/build"));
+app.get("/tfcadmin", (request, response) => {
+	response.sendFile(path.join(__dirname, "/staticRoutes/adminSite/build/index.html"));
+});
+
+app.use(express.static("staticRoutes/userSite/build"));
 app.get("/*", (request, response) => {
-	response.sendFile(path.join(__dirname, "/build/index.html"));
+	response.sendFile(path.join(__dirname, "/staticRoutes/userSite/build/index.html"));
 });
 
 const PORT = process.env.PORT;
